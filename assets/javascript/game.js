ca4d087d303566ab8947b4b9a7059ae0a86781b6
Rob Canfield <git@livesenjoyable.com>
2019-07-21 17:32:29 -0600

    Added psuedocode from class office hours. Wrote initial functions to randomly select a letter and genterate a user event of a key-press

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,87 @@
 // VARIABLES
+
 // • User guess
-var userGuess;
+var userGuess
+
 // • correct guess
 var correctGuess;
+
 // • incorrect guess
 var incorrectGuesses = [];
+
 // • guesses remaining
 var guessesRemaining = 9;
+
 // • wins
 var wins = 0;
+
 // • loses
 var loses = 0;
+
 // • computer guesses
-var computerLetter;
+var computerLetter = document.onkeyup = function(event) {
+    userGuess = event.key;
+};
+
 // • computer letters;
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+
+
+
 // DOM VARIABLES
+
 var winsDOM =
     document.getElementById("wins");
+
 var losesDOM =
     document.getElementById("loses");
+
 var guessesLeftDOM =
     document.getElementById("guess-count");
+
 var guessesLeftDOM =
     document.getElementById("guesses");
 
 
+
+
+
+
+
+
+
+ 
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    console.log(userGuess);
+}
+
+
+
+
+// FUNCTIONS
+// • Letter selection (computer)
 function generateLetter() {
     computerLetter = letters[Math.floor(Math.random() *
     letters.length)];
-    // console.log(computerLetter);
+    console.log(computerLetter);
 }
 
-// generateLetter();
+generateLetter();
+
+
+
+
+// • Create placeholder to display user guesses on screen
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    console.log(userGuess);
+}
+// • Decrement remaining guesses
+// • Check user guess with computer guess
+// • Update our DOM -->
 
 
 
